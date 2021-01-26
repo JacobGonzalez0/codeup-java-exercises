3e9818c5412b3415b21bb81220fe4c77381dcc90
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-26 14:34:29 -0600

    Added Factorial answer

---
+++ src/MethodsExercises.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(0, 10);
+        System.out.println(userInput);
+
+        System.out.println("The factorial of " + userInput + " is: " + Factorial(userInput));
+
+    }
+
+    public static int Addition(int a, int b){
+        return a + b;
+    }
+    public static int Subtraction(int a, int b){
+        return a - b;
+    }
+    public static int Multiplication(int a, int b){
+        return a * b;
+    }
+    public static int Division(int a, int b){
+        return a / b;
+    }
+    public static int Modulus(int a, int b){
+        return a % b;
+    }
+
+    public static int Factorial(int input){
+        int i, value = 1;
+        for(i = 1; i<=input;i++){
+            value = value * i;
+        }
+        return value;
+    }
+
+    public static int generateInteger(int min, int max){
+        int number = (int) Math.floor((Math.random() * max)) + min;
+        return number;
+    }
+
+    public static int getInteger(int min, int max){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+
+        if(n >= min && n <= max){
+            return n;
+        }else{
+            System.out.println("Please try again, 1 though 10: ");
+        }
+        return getInteger(min, max);
+        
+    }
+
+
+
+
+}
